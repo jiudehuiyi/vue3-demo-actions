2e3c5893ee6c87ea346f4ea8b39082b3f051fd27
jiudehuiyi <136859304@qq.com>
2024-07-04 11:36:04 +0800

    fix:修改文字

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!嘉慧444---wj---一个小娃娃--两个小娃娃" />
+      <HelloWorld msg="You did it!嘉慧444---wj---一个小娃娃--两个小娃娃--三个小娃娃" />
     </div>
   </header>
 
